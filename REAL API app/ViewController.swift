7d9849920c802b8df8f7e7e6b5aa3056efbaf68a
Lauren Carder <lcarder@laurens-macbook-air-9.local>
2019-02-27 11:49:11 -0600

    Parse JSON data

--- REAL API app/ViewController.swift
+++ REAL API app/ViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class CurrentlyViewController: UITableViewController {
+class MealViewController: UITableViewController {
     
-    var currently = [[String: String]]()
-    let apiKey = "05967568c1605ce17149c4b66b8f6c61"
+    var categories = [[String: String]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Weather Information"
-        let query = "https://api.darksky.net/forecast/05967568c1605ce17149c4b66b8f6c61/37.8267,-122.4233"
+        self.title = "Meal Categories"
+        let query = "https://www.themealdb.com/api/json/v1/1/categories.php"
+        if let url = URL(string: query)
+        {
+            if let data = try? Data(contentsOf: url)
+            {
+                let json = try! JSON(data: data)
+                if json["status"] == "ok"
+                {
+                    parse(json: json)
+                    return
+                }
+            }
+        }
+        loadError()
+    }
+    
+    func parse(json: JSON)
+    {
+        for result in json["categories"].arrayValue
+        {
+            let strCategory = result["strCategory"].stringValue
+            let strCategoryDescription = result["strCategoryDescription"].stringValue
+            let category = ["strCategory" : strCategory, "strCategoryDescription" : strCategoryDescription]
+           categories.append(category)
+        }
+        tableView.reloadData()
     }
 
+    func loadError()
+    {
+        let alert = UIAlertController(title: "Loading Error", message: "There was a problem loading the current weather", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
